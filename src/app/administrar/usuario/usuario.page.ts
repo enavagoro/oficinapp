0664b23e430b214dcbb7ebf036e2fed7c6cacb8e
enavagoro <enavagoro@gmail.com>
2020-03-17 19:34:42 -0300

    formulario usuario, boton clave, falta desactivar login usuarios estado 0

--- src/app/administrar/usuario/usuario.page.ts
+++ src/app/administrar/usuario/usuario.page.ts
@@ -10,173 +10,201 @@ import { UsuarioService, Usuario} from '../../_servicios/usuario.service';
 export class UsuarioPage implements OnInit {
   usuarios = [];
   public usuario : Usuario = {estado:0,id:0,nombre:'',apellido:'',correo:'',clave:''};
-
+  bandera = false;
+  flag = false;
+  
   constructor(private usuarioService : UsuarioService,
               public actionSheetController: ActionSheetController,
               private toastController : ToastController,
               private alertController :AlertController,
               private modalCtrl : ModalController) {}
 
-              ngOnInit() {
+  ngOnInit() {
 
-                this.usuarioService.listar().then(usuarios=>{
-                  usuarios.subscribe(u=>{
-                    this.usuarios= u;
-                  })
-                })
+    this.usuarioService.listar().then(usuarios=>{
+      usuarios.subscribe(u=>{
+      this.usuarios= u;
+      })
+    })
+  }
+   public guardarUsuario(){
+    //console.log('entra');
+    this.usuario.id = 0 + (this.usuarios.length + 1);
+    this.usuarioService.insertar(this.usuario).subscribe(usuario=>{
+      //console.log('entra2');
+      this.ngOnInit();
+      this.usuario = {estado:0,id:0,nombre:'',apellido:'',correo:'',clave:''};
+    })
 
-              }
+  }
 
-              public guardarUsuario(){
-                //console.log('entra');
-                this.usuario.id = 0 + (this.usuarios.length + 1);
-                this.usuarioService.insertar(this.usuario).subscribe(usuario=>{
-                  //console.log('entra2');
-                  this.ngOnInit();
-                  this.usuario = {estado:0,id:0,nombre:'',apellido:'',correo:'',clave:''};
-                })
+  public actualizarUsuario(){
+    this.usuarioService.actualizar(this.usuario.id,this.usuario).subscribe(usuario=>{
+      //console.log(usuario);
+      this.ngOnInit();
+      this.usuario = {estado:0,id:0,nombre:'',apellido:'',correo:'',clave:''};
+    })
+  }
+  public eliminacionLogica(){
+    this.usuarioService.borrar(this.usuario.id,this.usuario).subscribe(datos=>{
+      //console.log(datos);
+      this.ngOnInit();
+    })
+  }
+  public deshabilitarInputs(estado){
+    var form = document.querySelector('form');
+    for (let i=0; i<form.elements.length; i++)
+    {
+      (form.elements[i] as any).disabled=estado;
+    }
+  }
+  public cancelar(){
+    this.bandera=false;
+    this.deshabilitarInputs(false);
+    this.usuario = {estado:0,id:0,nombre:'',apellido:'',correo:'',clave:''};
+  }
+  async eliminar(opcion) {
+    //console.log(this.usuario);
 
-              }
+    const alert = await this.alertController.create({
+      header: 'Favor confirmar!',
+      message: 'Estas a punto de <br><strong>'+opcion+' UN USUARIO</strong>!!!',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            //console.log('Cancelado');
+          }
+        }, {
+          text: 'Okay',
+          handler: () => {
+            this.eliminacionLogica();
+          }
+        }
+      ]
+    });
 
-              public actualizarUsuario(){
-                this.usuarioService.actualizar(this.usuario.id,this.usuario).subscribe(usuario=>{
-                  //console.log(usuario);
-                  this.ngOnInit();
-                  this.usuario = {estado:0,id:0,nombre:'',apellido:'',correo:'',clave:''};
-                })
-              }
-              public eliminacionLogica(){
-                this.usuarioService.borrar(this.usuario.id,this.usuario).subscribe(datos=>{
-                  //console.log(datos);
-                  this.ngOnInit();
-                })
-              }
-              async eliminar(opcion) {
-                //console.log(this.usuario);
+    await alert.present();
+  }
+  async confirmarActualizar() {
+    //console.log(this.usuario);
 
-                const alert = await this.alertController.create({
-                  header: 'Favor confirmar!',
-                  message: 'Estas a punto de <br><strong>'+opcion+' UN USUARIO</strong>!!!',
-                  buttons: [
-                    {
-                      text: 'Cancelar',
-                      role: 'cancel',
-                      cssClass: 'secondary',
-                      handler: (blah) => {
-                        //console.log('Cancelado');
-                      }
-                    }, {
-                      text: 'Okay',
-                      handler: () => {
-                        this.eliminacionLogica();
-                      }
-                    }
-                  ]
-                });
+    const alert = await this.alertController.create({
+      header: 'Favor confirmar!',
+      message: 'Estas a punto de <br><strong>ACTUALIZAR UN USUARIO</strong>!!!',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            //console.log('Cancelado');
+          }
+        }, {
+          text: 'Okay',
+          handler: () => {
+            this.actualizarUsuario();
+          }
+        }
+      ]
+    });
 
-                await alert.present();
-              }
-              async confirmarActualizar() {
-                //console.log(this.usuario);
+    await alert.present();
+  }
+  async confirmar() {
+    //console.log(this.usuario);
 
-                const alert = await this.alertController.create({
-                  header: 'Favor confirmar!',
-                  message: 'Estas a punto de <br><strong>ACTUALIZAR UN USUARIO</strong>!!!',
-                  buttons: [
-                    {
-                      text: 'Cancelar',
-                      role: 'cancel',
-                      cssClass: 'secondary',
-                      handler: (blah) => {
-                        //console.log('Cancelado');
-                      }
-                    }, {
-                      text: 'Okay',
-                      handler: () => {
-                        this.actualizarUsuario();
-                      }
-                    }
-                  ]
-                });
+    const alert = await this.alertController.create({
+      header: 'Favor confirmar!',
+      message: 'Estas a punto de <br><strong>CREAR UN USUARIO</strong>!!!',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            //console.log('Cancelado');
+          }
+        }, {
+          text: 'Okay',
+          handler: () => {
+            this.guardarUsuario();
+          }
+        }
+      ]
+    });
 
-                await alert.present();
-              }
-              async confirmar() {
-                //console.log(this.usuario);
-
-                const alert = await this.alertController.create({
-                  header: 'Favor confirmar!',
-                  message: 'Estas a punto de <br><strong>CREAR UN USUARIO</strong>!!!',
-                  buttons: [
-                    {
-                      text: 'Cancelar',
-                      role: 'cancel',
-                      cssClass: 'secondary',
-                      handler: (blah) => {
-                        //console.log('Cancelado');
-                      }
-                    }, {
-                      text: 'Okay',
-                      handler: () => {
-                        this.guardarUsuario();
-                      }
-                    }
-                  ]
-                });
-
-                await alert.present();
-              }
-              async opciones(usuario) {
-                //console.log(usuario)
-                var opcion = "BORRAR";
-                if(usuario.estado == 0){
-                  opcion = "RECUPERAR"
-                }
-                const actionSheet = await this.actionSheetController.create({
-                  header: 'Albums',
-                  buttons: [{
-                    text: opcion,
-                    role: 'destructive',
-                    icon: 'trash',
-                    handler: () => {
-                      this.usuario = usuario;
-                      this.eliminar(opcion);
-                    }
-                  }, {
-                    text: 'Actualizar',
-                    icon: 'share',
-                    handler: () => {
-                      this.usuario = usuario;
-                      //console.log(usuario);
-                    }
-                  },{
-                    text: 'Duplicar',
-                    icon: 'heart',
-                    handler: () => {
-                      usuario.id == 0;
-                      this.usuario = usuario;
-                      this.usuario.id = 0;
-                      //console.log(this.usuario);
-                    }
-                  }, {
-                    text: 'Cancelar',
-                    icon: 'close',
-                    role: 'cancel',
-                    handler: () => {
-                      //console.log('Cancel clicked');
-                    }
-                  }]
-                });
-                await actionSheet.present();
-              }
-              filtrarUsuarios(){
-                var usuarios = [];
-                for(let i = 0 ; i < this.usuarios.length ; i ++){
-                  if(this.usuarios[i].estado){
-                    usuarios.push(this.usuarios[i]);
-                  }
-                }
-                return usuarios;
-              }
+    await alert.present();
+  }
+  async opciones(usuario) {
+    //console.log(usuario)
+    var opcion = "Borrar";
+    if(usuario.estado == 0){
+      opcion = "Recuperar"
+    }
+    this.deshabilitarInputs(false);
+    this.bandera=false;
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Opciones',
+      buttons: [{
+        text: 'Ver',
+        icon: 'eye',
+        handler: () => {
+          usuario.tipo=''+usuario.tipo;
+          this.usuario = usuario;
+          //console.log(usuario);
+          //console.log('bandera',this.bandera);
+          this.deshabilitarInputs(true);
+          this.bandera=true;
+        }
+      }, {
+        text: 'Actualizar',
+        icon: 'sync',
+        handler: () => {
+          this.bandera=false;
+          this.usuario = usuario;
+          //console.log(usuario);
+        }
+      },/*{
+        text: 'Duplicar',
+        icon: 'albums',
+        handler: () => {
+          this.bandera=false;
+          usuario.id == 0;
+          this.usuario = usuario;
+          this.usuario.id = 0;
+          //console.log(this.usuario);
+        }
+      },*/ {
+        text: opcion,
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          this.bandera=false;
+          this.usuario = usuario;
+          this.eliminar(opcion);
+        }
+      },{
+        text: 'Cancelar',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          //console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
+  }
+  filtrarUsuarios(){
+    var usuarios = [];
+    for(let i = 0 ; i < this.usuarios.length ; i ++){
+      if(this.usuarios[i].estado){
+        usuarios.push(this.usuarios[i]);
+      }
+    }
+    return usuarios;
+  }
 
 }
